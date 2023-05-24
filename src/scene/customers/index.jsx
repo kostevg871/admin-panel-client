c19836ae0490c5b00d0c9058c21940a696c50f07
kosteg <kostyrkoevgeniy@yandex.ru>
2023-05-24 19:51:25 +0300

    er

---
+++ src/scene/customers/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Header from "components/Header";
+import { useGetCustomersQuery } from "state/api";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+const Customers = () => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetCustomersQuery();
+  console.log(data);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "NAME",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Namber",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="CUSTOMERS" subtitle="List of Customers" />
+      <Box mt="40px" height="75vh">
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Customers;
